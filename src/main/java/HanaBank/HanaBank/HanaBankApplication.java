afac1a130923e2894d826345d0161f63ba44abc8
Yerim Seok <dddsyl98@gmail.com>
2024-07-12 15:22:49 +0900

    [Feat] Initial commit

---
+++ src/main/java/HanaBank/HanaBank/HanaBankApplication.java
@@ -0,0 +1,13 @@
+package HanaBank.HanaBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HanaBank/HanaBank/HanaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package HanaBank.HanaBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
