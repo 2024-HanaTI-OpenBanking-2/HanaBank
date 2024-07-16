3b09c756d379619cc7751eb05de7cd338e3a8b47
Yerim Seok <dddsyl98@gmail.com>
2024-07-16 14:30:46 +0900

    [Fix] Change transaction request parameter type close #11

--- src/main/java/HanaBank/HanaBank/controller/TransactionController.java
+++ src/main/java/HanaBank/HanaBank/controller/TransactionController.java
@@ -1,12 +1,19 @@
 package HanaBank.HanaBank.controller;
 
+import HanaBank.HanaBank.dto.AccountCiResponseDTO;
+import HanaBank.HanaBank.dto.BankStatementDTO;
+import HanaBank.HanaBank.dto.TransactionDTO;
 import HanaBank.HanaBank.entity.BankAccount;
 import HanaBank.HanaBank.entity.BankStatement;
 import HanaBank.HanaBank.service.BankService;
 import HanaBank.HanaBank.service.TransactionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +25,8 @@ public class TransactionController {
   @Autowired
   private TransactionService transactionService;
 
-  @GetMapping("/acnt_num")
-  public List<BankStatement> getStatementsByAccountNum(@RequestParam String accountNum) {
-    return transactionService.getStatementsByAccountNum(accountNum);
+  @PostMapping("/acnt_num")
+  public ResponseEntity<List<BankStatement>> getStatementsByAccountNum(@RequestBody TransactionDTO transactionDTO) {
+    return ResponseEntity.ok(transactionService.getStatementsByAccountNum(transactionDTO.getAccountNum()));
   }
 }
---
+++ src/main/java/HanaBank/HanaBank/dto/TransactionDTO.java
@@ -0,0 +1,13 @@
+package HanaBank.HanaBank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDTO {
+  private String accessToken;
+  private String accountNum;
+}
